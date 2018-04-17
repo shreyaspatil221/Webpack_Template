777071c5b207a2bf22eafc2dace699bb2e8d46c2
shreyaspatil221 <shreyaspatil.jio@gmail.com>
2018-04-17 10:52:38 +0530

    First commit

---
+++ app/javascripts/app.js
@@ -0,0 +1,15 @@
+// Import the page's CSS. Webpack will know what to do with it.
+import "../stylesheets/app.css";
+
+// Import libraries we need.
+import { default as Web3} from 'web3';
+import { default as contract } from 'truffle-contract'
+/*
+var newData;
+
+function storeData(data)
+{
+ newData=data;
+ alert("New Data Added"+newData);
+}
+*/
